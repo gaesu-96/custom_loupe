c0c1a7a34566af72809d69c316bdd2ba66755b37
Kamichanw <865710157@qq.com>
2025-05-16 12:22:05 +0800

    polish

--- scripts/train_pe_core.sh
+++ scripts/train_pe_core.sh
@@ -1,9 +1,10 @@
 export CATTINO_HOME=./results
 
 meow set shutdown-on-complete False
+meow set override-exist-tasks allow
 meow create \
     "python src/train.py" \
     --min-devices 4 --requires-memory-per-device 40000 --task-name "seg-alpha=0.3" \
     -- \
     stage=seg \
-    hydra.run.dir=\${run_dir}/\${task_name}/outputs
+    model.tversky_alpha=0.3
